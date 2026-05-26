cec93ed12e893f489b1d14c7656dfaa30f092d8d
praveenrao556@gmail.com <goud.sagar.t@gmail.com>
2026-05-26 11:18:03 +0530

    Initial clean terraform project

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "praveen-static-website-demo-12345"
+
+  tags = {
+    Name = "Static Website"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.website_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "../website/index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "css" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "style.css"
+  source       = "../website/style.css"
+  content_type = "text/css"
+}
